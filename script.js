7ae1f82101e5d632c6563e0f2bd9ad2b1e2f2f82
prashantpawarpr <prashantpawarpr@gmail.com>
2025-04-02 16:17:34 +0530

    final sign up page

---
+++ script.js
@@ -0,0 +1,8 @@
+const password = Document.getElementById("password");
+// console.log(password);
+const validatePassword = () => {
+  console.log("hello world");
+};
+// validatePassword(){
+//   console.log(password);
+// }
